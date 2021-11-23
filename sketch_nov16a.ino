e8a77ce2e17f43dc26d65a373c99f2f6c6b269a9
Tyler Sehon <tsehon@usc.edu>
2021-11-23 08:14:00 -0800

    adding sketch

---
+++ sketch_nov16a.ino
@@ -0,0 +1,24 @@
+#include <Brain/Brain.h>
+
+Brain brain(Mindflex);
+
+void setup() 
+{
+  /* Begin the hardware serial 
+  */
+  Serial.begin(9600);
+}
+
+void loop() 
+{   
+  /* @brief: Print out data
+  *
+  * The .readCSV() function returns a char* containing the most recent brain data, in the format:
+  * "signal strength, attention, meditation, delta, theta, low alpha, high alpha, low beta, high beta, low gamma, high gamma" 
+  */
+
+  if (brain.update())
+  {
+    Mindflex.println(brain.readCSV());
+  }
+}
