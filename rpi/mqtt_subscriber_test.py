4d159668ad4c40a943dd331e3a0f892cb0d65b57
Tyler Sehon <tsehon@usc.edu>
2021-11-30 15:53:19 -0800

    pubsub

--- rpi/mqtt_subscriber_test.py
+++ rpi/mqtt_subscriber_test.py
@@ -3,17 +3,18 @@ import time
 
 def on_connect(client, userdata, flags, rc):
     print("Connected to server with result code " + str(rc))
-    client.subscribe("pi/bci-data")
+    client.subscribe("pi/brainwaves")
 
 def on_message(client, userdata, msg):
     print(str(msg.payload, "utf-8"))
 
 if __name__ == '__main__':
     client = mqtt.Client()
-    client.on_messsage = on_message
     client.on_connect = on_connect
-    client.connect(host="eclipse.usc.edu", port=11000, keepalive=60)
-    client.loop_start()
+    client.on_messsage = on_message
+    client.connect(host="test.mosquitto.org", port=1883, keepalive=60)
+    client.loop_forever()
 
     while True:
         time.sleep(1)
+
--- rpi/rpi_publish.py
+++ rpi/rpi_publish.py
@@ -6,17 +6,20 @@ uno = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
 uno.reset_input_buffer()
 
 def on_connect(client, userdata, flags, rc):
-    print("Connected to server with result code " +str(rc))
+    if rc == 0:
+        print("Broker connection successful")
+    else:
+        print("Connection failed with result code: " +str(rc))
     
 if __name__ == '__main__':
     client = mqtt.Client()
     client.on_connect = on_connect
-    client.connect(host="eclipse.usc.edu", port=11000, keepalive=60)
-    client.loop_start()
+    client.connect(host="test.mosquitto.org", port=1883, keepalive=60)
+    client.loop_forever()
 
     while True:
         if uno.in_waiting > 0:
             data = uno.readline().decode('utf-8').rstrip()
-            client.publish("pi/bci-data", data)
+            client.publish("pi/brainwaves", data)
             print(data)
         time.sleep(1)
