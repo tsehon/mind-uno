69274b8af35443f5ce6e0c1b72b18d6edae70275
Tyler Sehon <tsehon@usc.edu>
2021-11-29 20:20:07 -0800

    testing..

---
+++ rpi/mqtt_subscriber_test.py
@@ -0,0 +1,20 @@
+import paho.mqtt.client as mqtt
+import time
+
+def on_connect(client, userdata, flags, rc):
+    print("Connected to server with result code " + str(rc))
+
+    client.subscribe("pi/bci-data")
+
+def on_message(client, userdata, msg):
+    print(str(msg.payload, "utf-8"))
+
+if __name__ == '__main__':
+    client = mqtt.Client()
+    client.on_messsage = on_message
+    client.on_connect = on_connect
+    client.connect(host="eclipse.usc.edu", port=11000, keepalive=60)
+    client.loop_start()
+
+    while True:
+        time.sleep(1)
