4cabeb55bd8e7d48cc25424a6401010dd338c18a
vmuljo <muljo@usc.edu>
2021-11-30 22:06:06 -0800

    Added callback code

--- rpi/mqtt_subscriber_test.py
+++ rpi/mqtt_subscriber_test.py
@@ -4,6 +4,10 @@ import time
 def on_connect(client, userdata, flags, rc):
     print("Connected to server with result code " + str(rc))
     client.subscribe("pi/brainwaves")
+    client.message_callback_add("pi/brainwaves", brainwaves_callback)
+
+def brainwaves_callback(client, userdata, message):
+    print("VM: " + str(message.payload, "utf-8"))
 
 def on_message(client, userdata, msg):
     print(str(msg.payload, "utf-8"))
